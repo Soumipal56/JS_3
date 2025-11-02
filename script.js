46fb3c52b8bbad67202d3bd2cf6572caa66b004f
Soumi Pal <soumiisc2020@gmail.com>
2025-11-02 16:01:12 +0530

    removed comment

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 // 1.
-// for(let i = 1; i <= 10; i++){
-//     console.log(i);
-// }    
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}    
 
 // 2.
-// for(let i = 1; i<=20; i++){
-//     if(i % 2 == 0){
-//         console.log(i);
-//     }
-// }
+for(let i = 1; i<=20; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
 
 // 3.
-// for(let i = 10; i>=1; i--){
-//     console.log(i);
-// }
+for(let i = 10; i>=1; i--){
+    console.log(i);
+}
 
 // 4. 
-// for(let i = 100; i<105; i++){
-//     console.log("yes");
-// }
+for(let i = 100; i<105; i++){
+    console.log("yes");
+}
 
 // 5. 
-// for(let i = 1; i<=10; i++){
-//     if(i % 2 == 0){
-//         console.log(`${i} - even`);
-//     }else{
-//         console.log(`${i} - odd`);
-//     }
-// } 1 - odd, 2- even 
+for(let i = 1; i<=10; i++){
+    if(i % 2 == 0){
+        console.log(`${i} - even`);
+    }else{
+        console.log(`${i} - odd`);
+    }
+} 
+// 1 - odd, 2- even 
 
 // 6.
 // let number = +prompt("Enter a number");
@@ -64,18 +65,18 @@
 // }
 
 // 8. 
-// for(let i=1; i<=10; i++){
-//     console.log(`5 * ${i} = ${5*i}`);
-// }
+for(let i=1; i<=10; i++){
+    console.log(`5 * ${i} = ${5*i}`);
+}
 
 // 9.
-// let count = 0;
-// for(let num=1; num<=15; num++){
-//     if(num > 8){
-//         count++;
-//     }
-// }
-// console.log("Numbers greater than 8:",count);
+let count = 0;
+for(let num=1; num<=15; num++){
+    if(num > 8){
+        count++;
+    }
+}
+console.log("Numbers greater than 8:",count);
 
 // 10.
 // let user = prompt("Give password");
